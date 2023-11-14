905eea9e5bf0c9ffe44ccd5a99684999fd7eafb7
kayla_a <namtiagna@gmail.com>
2023-11-13 20:58:34 -0600

    Update Java
    having trouble getting formula function to show up. Currently have error message in place but continue to only get error message

--- java.js
+++ java.js
@@ -1,51 +1,42 @@
     /* Form Input*/
-    let balance = calcForm.balance.value;
-    let annualRate = calcForm.interest.value;
-    let months = calcForm.intDuration.value;
-    let error = "All fields must be more than 0 and cannot be blank.";
-    let valid;
+    var balance = calcForm.balance.value;
+    var annualRate = calcForm.interest.value;
+    var months = calcForm.intDuration.value;
+    var error = "All fields must be more than 0 and cannot be blank.";
+    var color;
     
 function submitData() {
     
-    
     if (isNaN(balance) || isNaN(annualRate) || isNaN(months)) {
-        updateDOM(error, "red");
+        updateDOM("error", "red");
         return false;
     } 
     else if (balance <= 0 || annualRate <= 0 || months <= 0) {
-        updateDOM(error, "red");
+        updateDOM("error", "red");
         return false;
     }
      else {
-        updateDOM(valid, "green");
+        updateDOM("valid", "green");
         computeFutureValue(balance, annualRate, months);
     }
 }
 
 function updateDOM(value, color) {
     /*Error Output */
-    let error = "All fields must be more than 0 and cannot be blank.";
-    let valid;
-    
-    color = (document.getElementById("futureValue").style.color);
 
-        if (value === error) {
+        if (value === "error") {
             document.getElementById("futureValue").innerHTML = error;
             document.getElementById("futureValue").style.color = "red";
-        } else if (value === valid) {
+        } else {
             document.getElementById("futureValue").style.color = "green";
             computeFutureValue(balance, annualRate, months);
         }
     
 }
 
-
-
 function computeFutureValue(balance, annualRate, months) {
-    
         while (i=0, i <= months, i++) {
             x = ((balance * annualRate * months)/100);
-            
         } 
-        document.getElementById("futureValue").innerHTML;
+        x = document.getElementById("futureValue").innerHTML;
 }
